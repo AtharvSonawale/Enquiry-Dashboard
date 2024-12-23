77116a01c7df3080883000404b563c27a59a341e
Soham Shewde <sohamshevde0045@gmail.com>
2024-12-23 15:37:01 +0530

    added routes and enquiry dropdown

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import EnquiryPage from './Pages/EnquiryPage'
+import UploadDataPage from './Pages/UploadDataPage'
 
 function App() {
 
   return (
     <>
     <Navbar />
+    
+    <Routes>
+			      <Route path="/" element={<UploadDataPage />} />
+			      <Route path="/enquiry" element={<EnquiryPage />} />
+			</Routes>
     </>
   )
 }
---
+++ Frontend/src/Pages/UploadDataPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadDataPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UploadDataPage
